51a11b1d3c7836cc4668ec349903771e5f2a636d
“ <sachinrahul154@gmail.com>
2020-01-03 23:52:33 +0530

    "first commit"

--- src/test/java/org/hrms/base/BasePackage.java
+++ src/test/java/org/hrms/base/BasePackage.java
@@ -3,7 +3,6 @@ package org.hrms.base;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.Properties;
-
 import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -15,7 +14,6 @@ public class BasePackage {
 	 * WebDriver 
 	 * Properties 
 	 * Logs log4J,.log,log4j.properties,logger
-	 * 
 	 * Extent Reports 
 	 *  Mail
 	 */
