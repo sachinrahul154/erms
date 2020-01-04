46a7c947eccd8c969539099567a58839722f9334
“ <sachinrahul154@gmail.com>
2020-01-04 23:40:09 +0530

    "Latest Updates"

--- src/test/java/com/hrms/testcase/admin/LoginAdmin.java
+++ src/test/java/com/hrms/testcase/admin/LoginAdmin.java
@@ -1,18 +1,39 @@
 package com.hrms.testcase.admin;
 
 import org.hrms.base.BasePackage;
-import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import jdk.internal.org.jline.utils.Log;
 
 public class LoginAdmin extends BasePackage {
 
-@Test 
+@FindBy(how =How.XPATH,using="//input[@id='username']")
+static WebElement username;
+@FindBy(how =How.XPATH,using="//input[@id='Password']")
+static WebElement password;
+@FindBy(how =How.XPATH,using="//input[@name='login']")
+static WebElement login;
+
+@Test(groups= {"Smoke","Regression"})
 public void AdminLogin()
 {
- driver.findElement(By.xpath(OR.getProperty("admin_username"))).sendKeys("admin");
- log.info("username Entered");
- driver.findElement(By.xpath(OR.getProperty("admin_password"))).sendKeys("test@123");
- log.info("password entered");
- driver.findElement(By.xpath(OR.getProperty("admin_login"))).click();
+	PageFactory.initElements(driver,LoginAdmin.class);
+	log.info("Page Loaded Successfully");
+	username.sendKeys(OR.getProperty("admin"));
+	log.info("Username Entered Successfully");
+	password.sendKeys(OR.getProperty("password"));
+	log.info("Password Entered Successfully ");
+	login.click();
+	log.info("Info Submitted");
+	String Expected_Url=driver.getCurrentUrl();
+	log.info("Current Url is as"+Expected_Url);
+ 	String Actual_Url="http://localhost/erms/erms/admin/";
+ 	Assert.assertEquals(Actual_Url, Expected_Url);
 }
+
 }
--- src/test/java/org/hrms/base/BasePackage.java
+++ src/test/java/org/hrms/base/BasePackage.java
@@ -9,31 +9,26 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeTest;
+
 public class BasePackage {
 
 	/*
-	 * WebDriver 
-	 * Properties 
-	 * Logs log4J,.log,log4j.properties,logger
-	 * Extent Reports 
-	 *  Mail
+	 * WebDriver --- Done Properties ---- Done Logs
+	 * log4J,.log,log4j.properties,logger ---Done Extent Reports Mail
 	 */
 
-	public static WebDriver driver;
+	public  WebDriver driver;
 	public static Properties Config = new Properties();
 	public static Properties OR = new Properties();
 	public static FileInputStream fis;
-    public Logger log=Logger.getLogger(BasePackage.class);
-    
-	//public static Logger log = Logger.getLogger();
+	public Logger log = Logger.getLogger(BasePackage.class);
 	@BeforeTest
 	public void setup() throws Exception {
-		
 
 		try {
 			fis = new FileInputStream(System.getProperty("user.dir") + "\\src\\test\\Properties\\config.properties");
 			Config.load(fis);
-          // log.debug("log Initiated");
+			// log.debug("log Initiated");
 		} catch (FileNotFoundException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
@@ -46,8 +41,8 @@ public class BasePackage {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		if(Config.getProperty("browser").equalsIgnoreCase("chrome")) {
+
+		if (Config.getProperty("browser").equalsIgnoreCase("chrome")) {
 			System.setProperty("webdriver.chrome.driver",
 					System.getProperty("user.dir") + "\\src\\test\\Driver\\chromedriver.exe");
 			driver = new ChromeDriver();
@@ -56,6 +51,7 @@ public class BasePackage {
 			System.out.println("Not Found");
 		}
 	}
+
 	@AfterMethod
 	public void aftermethod() {
 		driver.quit();
